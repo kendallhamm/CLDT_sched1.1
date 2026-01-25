af967b7eaff4c0943fc9579272fc251f148d4c68
kendallhamm <kendallhamm17@gmail.com>
2026-01-25 15:49:00 -0500

    Update CLDT_schedule_app.py

--- CLDT_schedule_app.py
+++ CLDT_schedule_app.py
@@ -44,9 +44,15 @@ Squad integrity is preserved at all times.
 
 with st.expander("What's a lane vs a shift?"):
     st.markdown(r"""
-Definitions:
-Lane: A lane is generally going to be a 24-36 hour period, sometimes less sometimes more.
-Shift: Most lanes will have multiple shifts. Generally these break into a patrol base shift, a planning shift, and an actions on objective shift.
+**Definitions:**
+
+**Lane:**
+
+A lane is generally going to be a 24-36 hour period. These are set and distinguished by DMI. 
+
+**Shift:**
+
+Most lanes will have multiple shifts. Generally these break into a patrol base shift, a planning shift, and an actions on objective shift.
 """)
 
 
